f01c47a9f01cf72d6524b07ad201f60b7ebf2c1d
ct <frveket@126.com>
2019-06-12 21:55:05 +0800

    demo

--- src/js/entry/main.js
+++ src/js/entry/main.js
@@ -2,6 +2,11 @@ import '../../css/base'
 import App from '../module/App/main'
 import router from '../module/Route/main'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index';
+
+Vue.use(ElementUI);
+
 new Vue({
     el:'#app',
     router,
--- src/js/module/Designer/main.vue
+++ src/js/module/Designer/main.vue
@@ -1,5 +1,29 @@
 <template>
-    <span>设计器模块</span>
+    <div class="main-container">
+        <header class="header">
+            <span class="title">Vue组件页面设计器</span>
+            <div class="pull-right tool-btn">
+                <a class="btn btn-blue" id="preview_btn">预览</a>
+                <a class="btn btn-white" id="save_btn">保存</a>
+            </div>
+        </header>
+        <section class="section">
+            <div class="left-nav pull-left">
+                <ul class="nav-list">
+                    <li class="nav-header"><span>vue组件</span></li>
+                    <li class="list-item" id="component-def-list">
+                        <div class="widget-item component_js ui-draggable" type="def" title="拖拽至页面中间区域" component="MipHello"> <span>MipHello</span> </div>
+                        <div class="widget-item component_js ui-draggable" type="def" title="拖拽至页面中间区域" component="MipTest"> <span>MipTest</span> </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="wd-container">
+                <div id="widget-control" class="widget-control ui-sortable">
+
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
@@ -8,6 +32,10 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
+    .main-container{
+        height: 100%;
+        width: 100%;
+    }
 </style>
